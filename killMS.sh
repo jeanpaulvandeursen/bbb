2e333c1cc2bf744a81ea158eacf66895000fe75f
Jean Paul van Deursen <jean.paul.van.deursen@nl.abnamro.com>
2021-07-12 08:35:22 +0200

    removed sudo and -9 from kill command to run as normal user

--- killMS.sh
+++ killMS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for app in outlook teams;do sudo kill -9 `ps -ef | grep -i $app | awk 'NR==1{print $2}'`;done
+for app in outlook teams;do kill `ps -ef | grep -i $app | awk 'NR==1{print $2}'`;done
 if [[ $1 == "-h" ]]
 then
 	exit 1
