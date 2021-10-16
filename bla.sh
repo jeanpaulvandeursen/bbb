abd10564951a07465daeb126b18198958d5b700d
Jean-Paul van Deursen <github@vandeursen.org>
2021-10-16 15:09:00 +0200

    Delete bla.sh

--- bla.sh
+++
@@ -1,4 +0,0 @@
-GITHUB_API_TOKEN=$(security find-generic-password -s 'GitHub API Token Repos' -w)
-
-curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST https://api.github.com/user/repos -d '{"name":"blabla", "description": "blablabla"}' 
-# | jq '{id,name,created_at}'
