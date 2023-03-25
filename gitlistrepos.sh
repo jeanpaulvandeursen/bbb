98aeb31bda694fb94e48017d49c2dad76c6b8054
jeanpaulvandeursen <github@vandeursen.org>
2023-03-25 11:20:23 +0100

    changed github token alias in keychain

--- gitlistrepos.sh
+++ gitlistrepos.sh
@@ -1,3 +1,3 @@
-GITHUB_API_TOKEN=$(security find-generic-password -s 'GitHub API Token Repos' -w)
+GITHUB_API_TOKEN=$(security find-generic-password -s 'github' -w)
 
 curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/users/jeanpaulvandeursen/repos | jq '.[] | .name'
