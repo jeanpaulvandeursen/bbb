9786fbadfb08794ffbd37e463fd24c39d6e4ffdd
Jean-Paul van Deursen <github@vandeursen.org>
2023-05-23 18:54:44 +0200

    added test for token existence to token.sh

--- token.sh
+++ token.sh
@@ -14,7 +14,14 @@ then
 	security add-generic-password -a ${USER} -s ${TOKENID} -w ${TOKENVALUE}
 elif [[ $1 == "get" ]]
 then
-	security find-generic-password -s ${TOKENID} -w | pbcopy
+	TOKENVALUE=`security find-generic-password -s ${TOKENID} -w`
+	STATUS=$?
+	if [[ $STATUS -ne 0 ]]
+	then
+		exit $STATUS
+	else
+	echo $TOKENVALUE | pbcopy
+	fi
 elif [[ $1 == "del" ]]
 then
 	security delete-generic-password -s ${TOKENID}
